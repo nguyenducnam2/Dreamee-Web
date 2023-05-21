55e869f2c5f9a08dc6b032e2bf734a1ca555bd7a
Nguyễn Đức Nam <98157595+nguyenducnam2@users.noreply.github.com>
2023-05-21 17:29:06 +0700

    commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let panel = document.querySelector(".panel");
+
+hiddenPanel();
+
+function hiddenPanel() {
+    panel.style.display = "none";
+}
+
+function showPanel() {
+    panel.style.display = "block";
+}
